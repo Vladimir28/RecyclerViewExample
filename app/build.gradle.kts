e8b9a8acba09ab1eda25ac72140d6bf94357bfa2
Vladimir Teslya <vova.teslya@gmail.com>
2026-01-26 19:42:02 +0300

    update TaskItemAdapter.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     kapt(libs.androidx.room.compiler)
+
    // implementation(libs.androidx.room.compiler)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
